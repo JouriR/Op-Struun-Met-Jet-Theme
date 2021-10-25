0a50f81b9ea38d030d5cc235c07beb53ba2b2221
Jesse <jaschoonveld@st.noorderpoort.nl>
2021-10-25 14:50:17 +0200

    homepage meta tags en title

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="description" content="<?php bloginfo('description') ?>">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/style.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        <?php bloginfo('name'); ?> |
+        <?php is_front_page() ? bloginfo('description') : wp_title(); ?>
+    </title>
+    <?php wp_head(); ?>
+</head>
+<body>
+<div class="container">
+    <h1>Home</h1>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<footer>
+
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>

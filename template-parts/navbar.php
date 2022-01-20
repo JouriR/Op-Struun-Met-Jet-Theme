2c3268d382a03cc7f33e14702cf86da79434f437
Jesse <jaschoonveld@st.noorderpoort.nl>
2022-01-20 14:09:33 +0100

    navbar fix

--- template-parts/navbar.php
+++ template-parts/navbar.php
@@ -6,7 +6,7 @@
             }
             ?> navbar-expand-lg navbar-light  bg-light">
                 <div class="container-fluid p-0">
-                    <a class="navbar-brand p-3" href="#">Op Struun Met Jet</a>
+                    <a class="navbar-brand p-3" href="<?php echo home_url(); ?>">Op Struun Met Jet</a>
                     <button class="navbar-toggler m-2 mr-4" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -14,7 +14,7 @@
                         <div class="buffer d-none d-sm-block w-50"></div>
                         <ul class="navbar-nav d-flex justify-content-end ms-auto mb-2 mb-lg-0">
                             <li class="nav-item p-2 p-sm-2 p-xl-1">
-                                <a class="nav-link p-2 p-sm-2 p-xl-1 active" aria-current="page" href="../">Home</a>
+                                <a class="nav-link p-2 p-sm-2 p-xl-1 active" aria-current="page" href="<?php echo home_url(); ?>">Home</a>
                             </li>
                             <li class="nav-item p-2 p-sm-2 p-xl-1">
                                 <a class="nav-link p-2 p-sm-2 p-xl-1" href="./about">Over Mij</a>

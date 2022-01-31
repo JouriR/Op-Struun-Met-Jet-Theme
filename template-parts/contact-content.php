57c232f0eb951bafad5811e9b6fa9e1ed5c423c0
JSchoonveld <jesse.schoonveld@gmail.com>
2022-01-31 22:47:00 +0100

    Desktop versie contact pagina gemaakt en begin gemaakt met mobiel

--- template-parts/contact-content.php
+++ template-parts/contact-content.php
@@ -4,19 +4,84 @@
 get_template_part('template-parts/navbar');
 ?>
 <div class="contact-container content-container container">
-    <div class="row my-4">
-        <div class="col text-center">
-            <h1 class="text-uppercase fw-bold">Contact</h1>
-        </div>
-        <div class="col text-start">
-            <h1 class="text-uppercase fw-bold">Stuur een bericht</h1>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
+    <div class="row mt-4">
+        <div class="col-12 col-md-6 px-4 px-sm-0 left-section-contact">
+            <div class="row text-center text-md-start my-4">
+                <h2 class="text-uppercase fw-bold contact-title fw-bold">Contact</h2>
+            </div>
+            <div class="row my-4 d-flex">
+                <div class="col-md-4 col-5 justify-content-center col-lg-2 d-flex">
+                    <img class="float-start social-icons-contact my-auto"
+                         src="<?php echo get_template_directory_uri(); ?>/images/email.png" alt="email">
+                </div>
+                <div class="col-md-8 col-7 col-lg-10 d-flex">
+                    <div class="row my-auto">
+                        <span class="contact-social-bold-text">E-mail:</span>
+                        <span class="contact-social-subtext">
+                             <span class="orange-border-bottom">
+                           hvdnoord@ziggo.nl
+                             </span>
+                        </span>
+                    </div>
+
+                </div>
+            </div>
+            <div class="row my-4 d-flex">
+                <div class="col-md-4 col-5 justify-content-center col-lg-2 d-flex">
+                    <img class="float-start social-icons-contact my-auto"
+                         src="<?php echo get_template_directory_uri(); ?>/images/instagram.png" alt="instagram">
+                </div>
+                <div class="col-md-8 col-7 col-lg-10 d-flex">
+                    <div class="row my-auto">
+                        <span class="contact-social-bold-text">Instagram</span>
+                        <span class="contact-social-subtext">
+                             <span class="orange-border-bottom">
+                         @opstruunmetjet
+                             </span>
+                        </span>
+
+                    </div>
+
+                </div>
+            </div>
+            <div class="row my-4 d-flex">
+                <div class="col-md-4 col-5 justify-content-center col-lg-2 d-flex">
+                    <img class="float-start social-icons-contact my-auto"
+                         src="<?php echo get_template_directory_uri(); ?>/images/facebook.png" alt="facebook">
+                </div>
+                <div class="col-md-8 col-7 col-lg-10 d-flex">
+                    <div class="row my-auto">
+                        <span class="contact-social-bold-text">Facebook</span>
+                        <span class="contact-social-subtext">
+                             <span class="orange-border-bottom">
+                        Op Struun Met Jet
+                             </span>
+                        </span>
+                    </div>
+
+                </div>
+            </div>
+            <div class="row my-4 d-flex">
+                <div class="col-md-4 col-5 justify-content-center col-lg-2 d-flex">
+                    <img class="float-start social-icons-contact my-auto"
+                         src="<?php echo get_template_directory_uri(); ?>/images/phone.png" alt="phone">
+                </div>
+                <div class="col-md-8 col-7 col-lg-10 d-flex">
+                    <div class="row my-auto">
+                        <span class="contact-social-bold-text">Telefoon</span>
+                        <span class="contact-social-subtext">
+                        <span class="orange-border-bottom">06-20 18 77 97</span>
+                        </span>
+                    </div>
+
+                </div>
+            </div>
 
         </div>
-        <div class="col">
+        <div class="col-12 col-md-6 justify-content-center right-section-contact">
+            <div class="row my-4">
+                <h2 class="text-uppercase text-center text-md-start fw-bold contact-title fw-bold">Stuur een bericht</h2>
+            </div>
             <form>
                 <div class="mb-0">
                     <label for="inputEmail" class="form-label"></label>
@@ -28,9 +93,9 @@ get_template_part('template-parts/navbar');
                 </div>
                 <div class="mb-3">
                     <label for="inputBody" class="form-label"></label>
-                    <textarea class="p-2" placeholder="Bericht" name="body" id="inputBody" cols="79" rows="10"></textarea>
+                    <textarea class="p-2 mt-4 form-control" placeholder="Bericht"  name="body" id="inputBody" cols="80" rows="10"></textarea>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button class="homepage-button border-0 p-1">Verstuur</button>
             </form>
         </div>
     </div>

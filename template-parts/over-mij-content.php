7373eb9f88cfabc435c0601df4cec0bef86a8c1b
JouriR <jouri.roosjen@home.nl>
2022-02-01 11:18:58 +0100

    Over mij desktop finished

--- template-parts/over-mij-content.php
+++ template-parts/over-mij-content.php
@@ -3,26 +3,26 @@
 <?php
 get_template_part('template-parts/navbar');
 ?>
-<div class="about-container container">
+<div class="container">
     <?php if (have_posts()) : ?>
         <?php while (have_posts()) : the_post() ?>
 
-            <article>
+            <article class="mt-5">
                 <div class="row">
-                    <div class="col-6"></div>
-                    <div class="col-6">
+                    <div class="col-5"></div>
+                    <div class="col-7">
                         <h1><?php the_title() ?></h1>
                     </div>
                 </div>
 
                 <div class="row">
-                    <div class="col-6">
-                        <img class="" 
+                    <div class="col-5">
+                        <img class="imgAbout" 
                              src="<?= get_the_post_thumbnail_url() ?>" 
                              alt="Henriette"
                         >
                     </div>
-                    <div class="col-6">
+                    <div class="col-7">
                         <?= get_the_content() ?>
                     </div>
                 </div>

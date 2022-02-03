5432b797cf69459d3516b13b7aafde819453fb78
JouriR <jouri.roosjen@home.nl>
2022-02-03 09:20:25 +0100

    Fixed aanbod page

--- template-parts/aanbod-content.php
+++ template-parts/aanbod-content.php
@@ -2,56 +2,50 @@
 
 <?php
 get_template_part('template-parts/navbar');
+$image_thumbnail = get_the_post_thumbnail_url(); // Thumbnail niet meer beschikbaar na custom post loop
 ?>
 <div class="container">
-    <?php if (have_posts()) : ?>
-        <?php while (have_posts()) : the_post();
-            $url = get_the_post_thumbnail_url();?>
+    <article class="mt-5">
+        <div class="row">
+            <div class="col-7">
+                <h1><?php the_title() ?></h1>
+            </div>
+            <div class="col-5"></div>
+        </div>
 
-            <article class="mt-5">
-                <div class="row">
-                    <div class="col-7">
-                        <h1><?php the_title() ?></h1>
-                    </div>
-                    <div class="col-5"></div>
-                </div>
+        <div class="row">
+            <div class="col-7">
+                <?php $args = array(
+                    'post_type' => 'aanbod_post_type',
+                    'post_status' => 'publish',
+                    'order' => 'ASC',
+                );
+                $posts = new WP_Query($args);
+                if ($posts->have_posts()) :
+                    while ($posts->have_posts()) : $posts->the_post(); ?>
 
-                <div class="row">
-                    <div class="col-7">
-                        <?php $args = array(
-                            'post_type' => 'aanbod_post_type',
-                            'post_status' => 'publish',
-                            'order' => 'ASC',
-                        );
-                        $posts = new WP_Query($args);
-                        if ($posts->have_posts()) :
-                            while ($posts->have_posts()) : $posts->the_post(); ?>
+                        <details>
+                            <summary><?= the_title() ?></summary>
+                            <div class="detailContent">
+                                <p>
+                                    <?= the_content() ?>
+                                </p>
+                            </div>
+                        </details>
 
-                                <details>
-                                    <summary><?= the_title() ?></summary>
-                                    <div class="detailContent">
-                                        <p>
-                                            <?= the_content() ?>
-                                        </p>
-                                    </div>
-                                </details>
+                    <?php
+                    endwhile;
+                else : ?>
 
-                            <?php
-                            endwhile;
-                        else : ?>
+                    <h2 style="color: red;">Er is momenteel geen aanbod beschikbaar...</h2>
 
-                            <h2 style="color: red;">Er is momenteel geen aanbod beschikbaar...</h2>
-
-                        <?php endif; ?>
-                        <hr class="detailBottomBorder">
-                    </div>
-                    <div class="col-5">
-                        <img class="imgAanbod" src="<?= $url ?>" alt="Henriette">
-                    </div>
-                </div>
-            </article>
-
-        <?php endwhile; ?>
-    <?php endif; ?>
+                <?php endif; ?>
+                <hr class="detailBottomBorder">
+            </div>
+            <div class="col-5">
+                <img class="imgAanbod" src="<?= $image_thumbnail ?>" alt="Henriette">
+            </div>
+        </div>
+    </article>
 </div>
 <?php get_footer(); ?>
